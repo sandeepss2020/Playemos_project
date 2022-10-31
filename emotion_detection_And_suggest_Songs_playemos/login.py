e57086ac8e38ea119c622d765eef88128e823c1e
sandeepss2020 <sandeepss.christ@gmail.com>
2022-10-31 16:21:38 +0530

    Initial commit

---
+++ emotion_detection_And_suggest_Songs_playemos/login.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import cv2
+import sys
+
+# Create an empty container
+placeholder = st.empty()
+
+actual_email = "admin"
+actual_password = "playemos"
+
+# Insert a form in the container
+with placeholder.form("login"):
+    st.markdown("#### Enter your Email And Password")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Login")
+
+if submit and email == actual_email and password == actual_password:
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    st.success("Login successful")
+
+    cascPath = sys.argv[0]
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+elif submit and email != actual_email and password != actual_password:
+    st.error("Login failed")
+else:
+    pass
